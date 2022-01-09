a6194251afcb56ddc2bc598b6d1d26a875c8523a
Hassan Rifi laroussi <hrifi-la@fu-r9-p8.s19.be>
2022-01-09 18:51:06 +0100

    ft_strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hrifi-la <hrifi-la@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/09 18:34:51 by hrifi-la          #+#    #+#             */
+/*   Updated: 2022/01/09 18:50:45 by hrifi-la         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int		t_size;
+	int		i;
+	int		j;
+
+	t_size = ft_strlen(s1) + ft_strlen(s2);
+	str = malloc(sizeof(*s1) * t_size);
+	if (str == 0)
+		return (0);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		str[i] = s2[j];
+		j++;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
